431cab849277312a51bc65881a0b14fc2778b7d3
Gloeyisk <gloeyisk@gmail.com>
2019-06-05 15:15:33 +0700

    v1.7.1/171/Stable
    Changelogs:
    - Added native busybox detection
    - Added modified props
    - Update doze setup services
    - Dropped modified xml
    - Dropped optimizing GMS
    
    Signed-off-by: Gloeyisk <gloeyisk@gmail.com>

--- README.md
+++ README.md
@@ -1,34 +1,14 @@
 # Universal GMS Doze
-Optimized GMS and disabled Find My Device with additional modified services
+*Systemlessly prevent unnecessary GMS running in the background with additional modified services*
 
 ## Installation Guides
-- Either install as a Magisk module or your choice root methods (in favor of SuperSU or losSU with Init.d Injector installed)
-- Enforcing SELinux (optional)
-- Compatible with doze/hibernator apps (optional)
-
-## Troubleshooting
-- **Find My Device** would be turning back **Enabled** while the **Play services library** getting updates. For everyone who already joined Beta Tester for Google Play services preferably to **opt-out** or **Leave** those beta programs which offered daily-routine-update. And if the update already passed (without your involvement) simply **Clear Data** for both Play services and Play Store (optional), **Reboot** your phone and give your **Best Patience** for wait then re-check.
-- Providing **logs** is a suitable choice for reporting current **bugs**
-- **Old changes** can be found in support thread
-
-## Recent Changes
-### v1.7.0 (April 2019)
-- Bump to Imageless Template
-- Update Unity to v4.0
-- Sleep statements (80)
-- Exp: Doze setup services
-- Exp: Tunable network tweaks
-- Exp: Foreground schedtune
-- Exp: Partial Boeffla wakelock blocker
-- Removed old props
-- Removed logd
-- Update documentation
+- Either install as a Magisk module or your choice root methods (in favor of SuperSU or losSU with Init.d Injector).
+- SELinux Enforcing.
+- Compatible with doze/hibernator apps.
 
 ## Development Supports
-- [XDA Thread](https://forum.xda-developers.com/apps/magisk/module-universal-gms-doze-t3853710)
-- [GitHub](https://github.com/gloeyisk/UniversalGMSDoze)
-- [Telegram](https://t.me/GLdppg)
 - [Donate](https://paypal.me/gloeyisk)
-
-## Credits
+- [GitHub](https://github.com/gloeyisk/UniversalGMSDoze)
+- [Support](https://forum.xda-developers.com/apps/magisk/module-universal-gms-doze-t3853710)
+- [Telegram](https://t.me/GLdppc)
 - [Unity](https://github.com/Zackptg5/Unity)
--- common/service.sh
+++ common/service.sh
@@ -1,40 +1,48 @@
-# Universal GMS Doze
-# Copyright © 2018-2019 GL-DP, gloeyisk
-# License: GPL v2
+# This script will be executed in late_start service mode
+# More info in the main Magisk thread
 
-# Sleep statements (delay execution);
-sleep 80
+# Unset BusyBox variable (krasCGQ)
+unset BB;
 
-# Disable Find My Device (reboot once if it is does not disabled automatically, prefer to Troubleshooting section for more details);
-pm disable com.google.android.gms/com.google.android.gms.mdm.receivers.MdmDeviceAdminReceiver
+# Detect BusyBox locations and automatically use the detected one (krasCGQ)
+for i in /sbin /system/xbin /su/xbin; do
+	if [ -f $i/busybox ]; then
+		BB=$i/busybox;
+		NOBB=0;
+		break;
+	fi;
+done;
 
-# Doze setup services (experimental);
-settings put global device_idle_constants light_after_inactive_to=5000,light_pre_idle_to=10000,light_max_idle_to=86400000,light_idle_to=43200000,light_idle_maintenance_max_budget=20000,light_idle_maintenance_min_budget=5000,min_time_to_alarm=60000,inactive_to=120000,motion_inactive_to=120000,idle_after_inactive_to=5000,locating_to=2000,sensing_to=120000,idle_to=7200000,wait_for_unlock=true
+# Stop GMS and restart it on boot (dorimanx)
+if [ "$($BB pidof com.google.android.gms | wc -l)" -eq "1" ]; then
+	$BB kill $($BB pidof com.google.android.gms);
+fi;
+if [ "$($BB pidof com.google.android.gms.unstable | wc -l)" -eq "1" ]; then
+	$BB kill $($BB pidof com.google.android.gms.unstable);
+fi;
+if [ "$($BB pidof com.google.android.gms.persistent | wc -l)" -eq "1" ]; then
+	$BB kill $($BB pidof com.google.android.gms.persistent);
+fi;
+if [ "$($BB pidof com.google.android.gms.wearable | wc -l)" -eq "1" ]; then
+	$BB kill $($BB pidof com.google.android.gms.wearable);
+fi;
 
-# Enable foreground schedtune (experimental);
-echo "5" > /dev/stune/foreground/schedtune.boost
+# Sleep statements (delay execution)
+sleep 10;
 
-# Tunable network tweaks (experimental, currently stands for westwood);
-echo "128" > /proc/sys/net/core/netdev_max_backlog
-echo "0" > /proc/sys/net/core/netdev_tstamp_prequeue
-echo "0" > /proc/sys/net/ipv4/cipso_cache_bucket_size
-echo "0" > /proc/sys/net/ipv4/cipso_cache_enable
-echo "0" > /proc/sys/net/ipv4/cipso_rbm_strictvalid
-echo "0" > /proc/sys/net/ipv4/igmp_link_local_mcast_reports
-echo "24" > /proc/sys/net/ipv4/ipfrag_time
-echo "westwood" > /proc/sys/net/ipv4/tcp_congestion_control
-echo "1" > /proc/sys/net/ipv4/tcp_ecn
-echo "0" > /proc/sys/net/ipv4/tcp_fwmark_accept
-echo "320" > /proc/sys/net/ipv4/tcp_keepalive_intvl
-echo "21600" > /proc/sys/net/ipv4/tcp_keepalive_time
-echo "1" > /proc/sys/net/ipv4/tcp_no_metrics_save
-echo "1500" > /proc/sys/net/ipv4/tcp_probe_interval
-echo "48" > /proc/sys/net/ipv6/ip6frag_time
-
-# Enable partial Boeffla wakelock blocker (Experimental);
-echo "wlan_pno_wl;wlan_ipa;wcnss_filter_lock;[timerfd];hal_bluetooth_lock;IPA_WS;sensor_ind;wlan;netmgr_wl;qcom_rx_wakelock;SensorService_wakelock;tftp_server_wakelock;wlan_wow_wl;wlan_extscan_wl;" > /sys/class/misc/boeffla_wakelock_blocker/wakelock_blocker
+# Doze setup services (experimental)
+su -c "pm enable com.google.android.gms/.update.SystemUpdateActivity"
+su -c "pm enable com.google.android.gms/.update.SystemUpdateService"
+su -c "pm enable com.google.android.gms/.update.SystemUpdateService$ActiveReceiver"
+su -c "pm enable com.google.android.gms/.update.SystemUpdateService$Receiver"
+su -c "pm enable com.google.android.gms/.update.SystemUpdateService$SecretCodeReceiver"
+su -c "pm enable com.google.android.gsf/.update.SystemUpdateActivity"
+su -c "pm enable com.google.android.gsf/.update.SystemUpdatePanoActivity"
+su -c "pm enable com.google.android.gsf/.update.SystemUpdateService"
+su -c "pm enable com.google.android.gsf/.update.SystemUpdateService$Receiver"
+su -c "pm enable com.google.android.gsf/.update.SystemUpdateService$SecretCodeReceiver"
 
 # Executing...
-# Side note: Other services will be added/removed when needed
 # Done
-#
+
+exit 0;
